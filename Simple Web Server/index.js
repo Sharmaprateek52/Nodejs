337d40c8c0c6aeeb4cacdbd2dff667bebebe9c26
Sharmaprateek52 <prateekkr12@gmail.com>
2020-02-24 22:18:36 +0530

    Added changes

--- Simple Web Server/index.js
+++ Simple Web Server/index.js
@@ -1,1 +1,7 @@
 var http = require('http');
+http.createServer( (req,res) => {
+    res.writeHead(200,{'Content-Type' : 'text/plain'});
+    res.end('Hello Nodejs');
+}).listen(3000);
+
+console.log('Server is running on "http://127.0.0.1:3000/"');
