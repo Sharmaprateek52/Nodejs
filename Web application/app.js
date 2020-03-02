f688e6b9a6bf49d24ec3f20d4554ff676c5974f3
Sharmaprateek52 <prateekkr12@gmail.com>
2020-03-02 22:13:58 +0530

    Added contact page

--- Web application/app.js
+++ Web application/app.js
@@ -19,6 +19,38 @@ app.get('/about', (req,res) => {
     res.render('about');
 });  
 
+app.get('/contact', (req,res) => {
+    res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'prateekkr12@gmail.com',
+			pass: 's3xy4$500'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Prateek Sharma <prateekkr12@gmail.com>',
+		to: 'nikhilrocks71@gmail.com',
+		subject: 'Website Submission',
+		text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: '+info.response);
+			res.redirect('/');
+		}
+	});
+});
+
 
 app.listen(3000,()=>{
     console.log("Server is live on port 3000!");
